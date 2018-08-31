d76486eb5f6e2eb03d27fb90a67a6706e4f395b5
darView <g.scholtes@darview.com>
2018-08-31 08:11:10 +0200

    fixes typo

--- src/js/gmap.js
+++
@@ -1,7 +0,0 @@
-function initMap()
-{
-    var location = {lat: 52.349220, lng: 4.789619};
-    var map      = new google.maps.Map(
-        document.getElementById('map'), {zoom: 10, center: location});
-    var marker   = new google.maps.Marker({position: location, map: map});
-}
